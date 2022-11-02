f04795be717a1951c112fcb178f0a5a273a774f3
harman127 <37600124+harman127@users.noreply.github.com>
2022-11-01 22:10:34 -0400

    Corrected github commit issue.

--- ticket/list.php
+++ ticket/list.php
@@ -1,28 +1,5 @@
 <?php
 require("../masterutil.php");
-<<<<<<< Updated upstream
-$pagename = "Dashboard";
-$tickets = new ticket();
-$unassigned = $tickets->GetUnassignedTickets();
-$unassigned_count = count($unassigned);
-?>
-<?php require($CONST_TEMPLATEDIR."/base_logged_in_top.php"); ?>
-	
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#my_tickets" type="button" role="tab" aria-selected="true">My Tickets</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link"  data-bs-toggle="tab" data-bs-target="#all_tickets" type="button" role="tab">All Tickets</button>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="my_tickets" role="tabpanel">abc</div>
-  <div class="tab-pane fade" id="all_tickets" role="tabpanel">123</div>
-</div>
-
-<?php require($CONST_TEMPLATEDIR."/base_logged_in_bottom.php"); ?>
-=======
 
 $pagename = "View Tickets";
 ?>
@@ -49,7 +26,7 @@ $pagename = "View Tickets";
                         <small><?= $r["Status_Name"]; ?></small>
                     </div>
                     <p class="mb-1"><?= $r["Description"]; ?></p>
-                    <small>
+                    <small> 
                         Submitted By <?= $r["Last_Name"].", ".$r["First_Name"] ?>
                     </small>
                 </a>
@@ -66,4 +43,3 @@ $pagename = "View Tickets";
 
 
 <?php require($CONST_TEMPLATEDIR."/base_logged_in_bottom.php"); ?>
->>>>>>> Stashed changes
