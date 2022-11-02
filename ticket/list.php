647f12a0d7ad6cfc193f721ad82eab661e41bdb2
jrfarl <jrfarl0545@ung.edu>
2022-11-01 20:15:32 -0400

    Added Content to list.php

--- ticket/list.php
+++ ticket/list.php
@@ -1,10 +1,61 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+require("masterutil.php");
+$pagename = "View Tickets";
+?>
+<?php require("templates/base_logged_in_top.php"); ?>
 
-<body>
-</body>
-</html>
+
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Untitled Document</title>
+    </head>
+
+    <body>
+
+        <div class="list-group">
+
+        <?php 
+            global $database;
+			
+           $sql = "Use SeniorProject;
+                    SELECT Title, Description, Status_Name, Last_Name, First_Name 
+                    FROM Tickets
+                    INNER JOIN Status on Tickets.Status_Code = Status.Status_Code
+                    INNER JOIN Users on Tickets.Created_By_ID = Users.User_ID";
+            $result = $database->query($sql);
+
+           if (mysqli_num_rows($result) > 0) { ?>
+
+                <?php  while($row = mysqli_fetch_assoc($result)) 
+                { ?>
+                    <!-- row 01 -->
+
+                    <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1"><?php echo $row["Title"]; ?></h5>
+                        <small><?php echo $row["Status_Name"]; ?></small>
+                    </div>
+                    <p class="mb-1"><?php echo $row["Description"]; ?></p>
+                    <small>
+                        Submitted By <?php echo $row["Last_Name"]; 
+                        ?>, <?php echo $row["First_Name"]; ?>
+                    </small>
+                </a>
+    
+                <? }?>
+
+          <?php
+          } else 
+            {
+              echo "0 results";
+          }
+          ?>
+
+        </div>
+
+    </body>
+    </html>
+
+<?php require("templates/base_logged_in_bottom.php"); ?>
