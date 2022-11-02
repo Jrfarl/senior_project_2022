38b6f6a801e90b1a26f91b7e2a7a53034656fbd5
harman127 <37600124+harman127@users.noreply.github.com>
2022-11-01 22:09:40 -0400

    Updated ticket listing

--- ticket/list.php
+++ ticket/list.php
@@ -22,3 +22,48 @@ $unassigned_count = count($unassigned);
 </div>
 
 <?php require($CONST_TEMPLATEDIR."/base_logged_in_bottom.php"); ?>
+=======
+
+$pagename = "View Tickets";
+?>
+<?php require($CONST_TEMPLATEDIR."/base_logged_in_top.php"); ?>
+
+        <div class="list-group">
+
+        <?php 
+			
+           $sql = "SELECT Title, Description, Status_Name, Last_Name, First_Name 
+                   FROM Tickets
+                   INNER JOIN Status on Tickets.Status_Code = Status.Status_Code
+                   INNER JOIN Users on Tickets.Created_By_ID = Users.User_ID";
+            $result = $database->query($sql);
+
+            if(isset($result) && !empty($result)) { ?>
+
+                <?php  foreach($result as $r){ ?>
+                    <!-- row 01 -->
+
+                    <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1"><?= $r["Title"]; ?></h5>
+                        <small><?= $r["Status_Name"]; ?></small>
+                    </div>
+                    <p class="mb-1"><?= $r["Description"]; ?></p>
+                    <small>
+                        Submitted By <?= $r["Last_Name"].", ".$r["First_Name"] ?>
+                    </small>
+                </a>
+    
+                <?php }?>
+
+          <?php
+          } else {
+              echo "0 results";
+          }
+          ?>
+
+        </div>
+
+
+<?php require($CONST_TEMPLATEDIR."/base_logged_in_bottom.php"); ?>
+>>>>>>> Stashed changes
