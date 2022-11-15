c2ea6206f7a5d7e29e8f3986edafebc392aae847
harman127 <37600124+harman127@users.noreply.github.com>
2022-11-14 22:52:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-sudo apt-get install mysql-server –y 
+sudo apt-get install mysql-server -y
 
-sudo apt install software-properties-common apt-transport-https –y 
+sudo apt install software-properties-common apt-transport-https -y
 
-sudo add-apt-repository ppa:ondrej/php -y 
+sudo add-apt-repository ppa:ondrej/php -y
 
-Sudo apt update && apt upgrade –y 
+Sudo apt update && apt upgrade -y 
 
-sudo apt install php8.0 php8.0-common libapache2-mod-php8.0 php8.0-cli php8.0-mysql –y 
+sudo apt install php8.0 php8.0-common libapache2-mod-php8.0 php8.0-cli php8.0-mysql -y
 
 mv senior_project_2022/* /var/www/html/
 
