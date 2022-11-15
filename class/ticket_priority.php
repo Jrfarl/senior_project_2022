d6bda9972aff57210affc4bd4cf127fc428e0ff0
harman127 <37600124+harman127@users.noreply.github.com>
2022-11-15 08:44:15 -0500

    Admin settings - Final Push

--- class/ticket_priority.php
+++ class/ticket_priority.php
@@ -10,4 +10,27 @@ class ticket_priority{
 	function GetAllPriorityNames(){
 		return $this->db->query("SELECT Priority_Level, Priority_Name FROM Priority");
 	}
+	
+	function CreatePriority($code, $name){
+		$return = $this->db->query("INSERT INTO `Priority` (Priority_Level, Priority_Name) VALUES (?,?)", [$code, $name], false);
+		return $return;
+	}
+	
+	function UpdatePriority($code, $name){
+		$return = $this->db->query("UPDATE `Priority` SET ? = ? WHERE `Priority_Level` = ?", [$code, $name], false);
+		return $return;
+	}
+	
+	function DeletePriority($ID){
+		$return = $this->db->query("DELETE FROM `Priority` WHERE `Priority_Level` = ?", [$ID], false);
+		return $return;
+	}
+	function GetAll(){
+		$priority_array = [];
+		$return = $this->db->query("SELECT * FROM Priority");
+		foreach($return as $r){
+			$priority_array[$r['Priority_Level']] = $r['Priority_Name'];
+		}
+		return $priority_array;
+	}
 }
